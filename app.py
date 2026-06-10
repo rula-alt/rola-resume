6fde232ad782551199fde6e7d4e7e8bb70e92524
rula-alt <rollaagh@gmail.com>
2026-06-10 13:25:20 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=true)
